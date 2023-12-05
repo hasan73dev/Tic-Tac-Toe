7ec52a9230dd6a5e154b0647606285204a03a34a
hasan73dev <hasanjabbarli82@gmail.com>
2023-12-05 08:19:20 +0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React,{useState} from 'react';
 import './App.css';
 import WeatherCard from './component/WeatherCard';
-import { apiKey } from './wheather';
 
 
-console.log("bir gun")
 type dataProp = {
   main:{
     temp: number
@@ -12,25 +10,15 @@ type dataProp = {
 }
 function App() {
 
-const [data,setData] = useState<dataProp>()
 
-const SearchCity = (e:React.KeyboardEvent<HTMLInputElement>) =>{
-  if(e.key === "Enter" && e.currentTarget.value !== ""){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.currentTarget.value}&appid=${apiKey}&units=metric`)
-    .then(res => res.json())
-    .then(data => setData(data))
-  }
-}
 
 
   return (
     <div className="App">
-      <WeatherCard SearchCity = {SearchCity} data = {data}/>
+      
     </div>
   );
 }
 
 export default App;
 
-
-///dfdsfds
